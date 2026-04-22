1e0a942308e2d0a11400e1046b3a4aff407b32a4
ShigureJishin <chungten2000@gmail.com>
2026-04-23 01:27:16 +0800

    feat: Redefine Vote database models

--- backend/SongRater.Api/Models/Vote.cs
+++ backend/SongRater.Api/Models/Vote.cs
@@ -1,6 +1,14 @@
-﻿namespace SongRater.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SongRater.Api.Models
 {
-    public class Class
+    public class Vote
     {
+        [Key]
+        public int Id { get; set; }
+        public int SongId { get; set; } // 關聯到哪一首歌
+        public string VoterFingerprint { get; set; } = string.Empty; // 防重複投票的觀眾 ID
+        public int Score { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
     }
 }
