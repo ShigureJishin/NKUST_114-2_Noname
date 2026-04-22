5963b7cd6df38cb36bea416f5b3490738807a917
ShigureJishin <chungten2000@gmail.com>
2026-04-23 01:43:41 +0800

    feat: implement AppDbContext for SQLite integration

---
+++ backend/SongRater.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SongRater.Api.Models;
+using System.Collections.Generic;
+
+namespace SongRater.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // 定義資料表
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+    }
+}
